0483a61b71b608834f433851d931e4aadf0eb151
Kutuzov Dmitry <archie-woc@ya.ru>
2022-03-15 02:37:01 +0300

    Add mailer functionality

--- Units/Mailer.js
+++ Units/Mailer.js
@@ -1,20 +1,42 @@
+import { WeatherDataProvider } from "../DataProvider/WeatherDataProvider.js";
+import { UserDataProvider } from "../DataProvider/UserDataProvider.js";
+import { OW_TOKEN } from "../secret_keys.js";
+
+const WeatherDP = new WeatherDataProvider();
+WeatherDP.Init(OW_TOKEN);
+const UserDP = new UserDataProvider();
+
 export default class Mailer {
-  constructor() {
+  constructor(bot) {
     this.CityList = [];
+    this.bot = bot;
   }
 
   launch() {
+    const now = new Date();
     const mailingDate = new Date();
-    mailingDate.setDate(new Date().getDate() + 1);
-    mailingDate.setHours(7);
-    setTimeout(mailingDate.now() - new Date().now());
-    setInterval(() => {
-      this.UpdateList();
-      this.SendDistribution();
-    }, 24 * 3600 * 1000);
+    mailingDate.setDate(now.getDate() + 1)
+    mailingDate.setHours(6);
+    mailingDate.setMinutes(0);
+    const timeoutTime = mailingDate.getTime() - now.getTime();
+
+    setTimeout(() => {
+      this.UpdateList().then(() => this.SendDistribution());
+      setInterval(() => {
+        this.UpdateList().then(() => this.SendDistribution());
+      }, 24 * 3600 * 1000 );
+    }, timeoutTime);
   }
 
-  UpdateList() { }
+  async UpdateList() {
+    this.CityList = await UserDP.GetAllCities();
+  }
 
-  SendDistribution() { }
+  SendDistribution() {
+    this.CityList.map(async (city) => {
+      const weatherData = await WeatherDP.GetWeatherData(city);
+      const users = await UserDP.GetUsersByCity(city);
+      users.map((user) => this.bot.telegram.sendMessage(user.tg_id, `Temperature in ${city}: ${weatherData.temp}℃\nFeels like: ${weatherData.feels_like}℃`));
+    });
+  }
 }
