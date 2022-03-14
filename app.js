102e307c1489c77d1b0c024ff61f4e15e519e7da
Kutuzov Dmitry <archie-woc@ya.ru>
2022-03-15 02:37:53 +0300

    Fix bugs and add mailer to main

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-import axios from 'axios';
 import { Telegraf } from 'telegraf';
-import UserDataProvider from './DataProvider/UserDataProvider.js';
+import { UserDataProvider } from './DataProvider/UserDataProvider.js';
+import { WeatherDataProvider } from './DataProvider/WeatherDataProvider.js';
 import { MDB_URL } from './secret_keys.js';
 import { TG_TOKEN } from './secret_keys.js';
 import { OW_TOKEN } from './secret_keys.js';
+import Mailer from './Units/Mailer.js';
 
-const OPEN_WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather';
+const UserDP = new UserDataProvider();
+const WeatherDP = new WeatherDataProvider();
 
-UserDataProvider.Init(MDB_URL);
+UserDP.Init(MDB_URL);
+WeatherDP.Init(OW_TOKEN);
 
 const bot = new Telegraf(TG_TOKEN);
+const mailer = new Mailer(bot);
 
 bot.start((ctx) => {
   const userId = ctx.message.chat.id;
-  UserDataProvider.AddUser(userId);
+  UserDP.AddUser(userId);
   ctx.telegram.sendMessage(ctx.message.chat.id, 'Hello, welcome to weather notifier bot.\nTo start getting weather notifications about cities, type:\n/add_city <Your city>\nTo stop getting notifications type:\n/stop\nTo get current information about weather type:\n/get_weather');
 });
 
 bot.command('add_city', (ctx) => {
   const city = ctx.message.text.slice('/add_city '.length);
-  UserDataProvider.AddCityToUser(ctx.message.from.id, city);
+  UserDP.AddCityToUser(ctx.message.from.id, city);
 });
 
 bot.command('stop', (ctx) => {
-  UserDataProvider.DeleteUser(ctx.message.from.id);
+  UserDP.DeleteUser(ctx.message.from.id);
 });
 
 bot.command('get_weather', (ctx) => {
-  UserDataProvider.GetUserById(ctx.message.from.id).then((user) => {
+  UserDP.GetUserById(ctx.message.from.id).then((user) => {
     const {cities} = user;
-    cities.map((city) => {
-      axios.get(OPEN_WEATHER_API, {
-        params: {
-          q: city,
-          appid: OW_TOKEN,
-          units: 'metric',
-        },
-      }).then((res) => {
-        console.log(res);
-        ctx.telegram.sendMessage(ctx.message.from.id, `Temperature in ${city}: ${res.data.main.temp}\n Feels like: ${res.data.main.feels_like}`);
-      }).catch((err) => {
-        ctx.telegram.sendMessage(ctx.message.from.id, `Could not get information about: ${city}`);
-        console.error(err);
-      });
+    cities.map(async (city) => {
+      const weatherData = await WeatherDP.GetWeatherData(city);
+      const messageText = `Temperature in ${city}: ${weatherData.temp}\nFeels like: ${weatherData.feels_like}`;
+      ctx.telegram.sendMessage(ctx.message.from.id, messageText);
     });
   });
 });
 
 bot.launch();
+mailer.launch();
